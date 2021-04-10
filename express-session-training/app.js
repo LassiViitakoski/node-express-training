aa483c3941289af7760ca91589090ceecd744069
LassiViitakoski <lassi.viitaokski@gmail.com>
2021-04-11 01:20:28 +0300

    session-kokeilut tehty

--- express-session-training/app.js
+++ express-session-training/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
 const session = require('express-session');
 const mysql_store = require('express-mysql-session')(session);
 
@@ -11,25 +10,36 @@ const options = {
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT
+    port: process.env.MYSQL_PORT,
+    clearExpired: true,
+    checkExpirationInterval: 1000 * 60,
+    expiration: 1000 * 60 * 60
 }
-
-const connection = mysql.createConnection(options);
-const session_store = new mysql_store({
-    createDatabaseTable: true,
-    expiration: 6000000
-}, connection);
+const session_store = new mysql_store(options);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
-    key: process.env.SESSION_KEY,
     secret: process.env.SESSION_SECRET,
     store: session_store,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 6
+    }
 }));
+
+app.get('/', (req, res) => {
+    console.log(req.session);
+    let session = req.session;
+    if(session.email) return res.send('email session : ' + session.email);
+    res.send('emailia ei löytynyt sessionista');
+})
+app.get('/setsession', (req, res) => {
+    req.session.email = 'lassi.viitakoski@gmail.com';
+    res.send('session asetettu');
+})
 
 const server = app.listen(8080, () => {
     console.log("Server running on port : " + server.address().port);
